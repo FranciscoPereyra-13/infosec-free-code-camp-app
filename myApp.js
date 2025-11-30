6d32ff7aae17afe66e2940c9853c3092aa0aff39
Francisco Pereyra <francisco.pereyra.dmc3@gmail.com>
2025-11-30 19:20:45 -0300

    feat: Desafío 1 - Implementar Helmet

--- myApp.js
+++ myApp.js
@@ -2,12 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-
-app.listen(PORT, () => 
-  {
-    console.log(`🥦 Useful programmer Info Security App Started on Port ${PORT}`);
-  });
+app.use(helmet());
 
 
 
@@ -63,5 +58,7 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`🥦 Useful programmer Info Security App Started on Port ${port}`);
 });
+
+
